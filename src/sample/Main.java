a9021b902d5acf8bf7c914207c8ea28ddaddc7e6
dainguyen <nguyendai1992@gmail.com>
2019-01-13 22:03:15 +0700

    Fixbug and update screen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 640, 400));
         primaryStage.show();
